18bb84af87938b84952b78c3867060c196e215a0
Zhiting Zhu <zhitingz@cs.utexas.edu>
2021-08-20 00:29:29 -0500

    add java docker files
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ java-dev/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_PATH=$(readlink -f $0)
+BASE_DIR=$(dirname $SCRIPT_PATH)
+
+JDK8_DEB_URL="https://corretto.aws/downloads/latest/amazon-corretto-8-x64-linux-jdk.deb"
+JDK11_DEB_URL="https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.deb"
+MAVEN_URL="https://downloads.apache.org/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.tar.gz"
+
+export DOCKER_BUILDKIT=1
+
+# ( cd $BASE_DIR &&
+#   docker build -t photoszzt/java-dev:jdk8-idea \
+#     --build-arg "JDK_DEB_URL=${JDK8_DEB_URL}" \
+#     --build-arg "JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto" \
+#     --build-arg "MAVEN_URL=${MAVEN_URL}" \
+#     --build-arg "USER_UID=$(id -u)" \
+#     -f Dockerfile . )
+# 
+# docker push photoszzt/java-dev:jdk8-idea
+
+( cd $BASE_DIR &&
+  docker build -t photoszzt/java-dev:jdk11-idea \
+    --build-arg "JDK_DEB_URL=${JDK11_DEB_URL}" \
+    --build-arg "JAVA_HOME=/usr/lib/jvm/java-11-amazon-corretto" \
+    --build-arg "MAVEN_URL=${MAVEN_URL}" \
+    --build-arg "USER_UID=$(id -u)" \
+    -f Dockerfile . )
+
+# docker push photoszzt/java-dev:jdk11-idea
